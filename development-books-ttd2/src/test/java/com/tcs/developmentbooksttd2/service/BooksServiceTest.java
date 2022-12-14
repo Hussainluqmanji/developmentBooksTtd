020a563d9ec06962d4b875425e66878362d6dd56
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-14 10:26:53 +0530

    refactor: segregate service object creation for test cases.

--- development-books-ttd2/src/test/java/com/tcs/developmentbooksttd2/service/BooksServiceTest.java
+++ development-books-ttd2/src/test/java/com/tcs/developmentbooksttd2/service/BooksServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.tcs.developmentbooksttd2.model.Books;
@@ -11,17 +12,22 @@ import com.tcs.developmentbooksttd2.model.BooksInput;
 
 public class BooksServiceTest {
 
+	BooksService service;
+
+	@BeforeEach
+	public void setUp() {
+		service = new BooksService();
+	}
+	
 	@Test
 	public void getAllBooks()
 	{
-		BooksService service = new BooksService();
 		List<Books> books = service.getAllBooks();
 		assertEquals(5, books.size());
 	}
 	
     @Test
     public void buyBookSuccess() {
-        BooksService service = new BooksService();
         double result = service.buyBooks(new BooksInput(1, 1));
         assertEquals(50.0, result, 0.0);
 
@@ -29,7 +35,6 @@ public class BooksServiceTest {
     
     @Test
     public void buyMultipleCopliesOfSameBooks() {
-        BooksService service = new BooksService();
         double result = service.buyBooks(new BooksInput(1, 5));
         assertEquals(250.0, result, 0.0);
 
