a2f99e567d101adf4615ace215fd9d07ce5e26d0
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-14 14:22:47 +0530

    refactor: complete price summary with actual price, final price and total books bought added.

---
+++ development-books-ttd2/src/main/java/com/tcs/developmentbooksttd2/model/PriceSummary.java
@@ -0,0 +1,22 @@
+package com.tcs.developmentbooksttd2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PriceSummary {
+
+	private int totalBooks;
+
+	private double actualPrice;
+
+	private double totalDiscount;
+
+	private double finalPrice;
+
+}
